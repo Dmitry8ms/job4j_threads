a182855f923973c5c5825a2be940f1b8cafcae9d
Dmitry8ms <dshatokhin@yandex.ru>
2022-08-18 21:18:42 +0700

    3. Прерывание нити [#1019 #333682]

--- src/main/java/ru/job4j/concurrent/ConsoleProgress.java
+++ src/main/java/ru/job4j/concurrent/ConsoleProgress.java
@@ -1,34 +1,20 @@
 package ru.job4j.concurrent;
 
 public class ConsoleProgress implements Runnable {
-
+    public String[] process = new String[] {"-", "\\", "|", "/"};
     @Override
     public void run() {
             while (!Thread.currentThread().isInterrupted()) {
-                try {
-                    Thread.sleep(500);
-                } catch (InterruptedException e) {
-                    Thread.currentThread().interrupt();
+                for (int i = 0; i < 4; i++) {
+                    System.out.print("\rLoading : " + process[i]);
+                    try {
+                        Thread.sleep(500);
+                    } catch (InterruptedException e) {
+                        Thread.currentThread().interrupt();
+                    }
                 }
-                System.out.print("\rLoading : " + "-");
-                try {
-                    Thread.sleep(500);
-                } catch (InterruptedException e) {
-                    Thread.currentThread().interrupt();
-                }
-                System.out.print("\rLoading : " + "\\");
-                try {
-                    Thread.sleep(500);
-                } catch (InterruptedException e) {
-                    Thread.currentThread().interrupt();
-                }
-                System.out.print("\rLoading : " + "|");
-                try {
-                    Thread.sleep(500);
-                } catch (InterruptedException e) {
-                    Thread.currentThread().interrupt();
-                }
-                System.out.print("\rLoading : " + "/");
+
             }
+
     }
 }
