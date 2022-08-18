768d2366f63f8eadb02d71848874e9821aa7f7f0
Dmitry8ms <dshatokhin@yandex.ru>
2022-08-18 12:36:07 +0700

    1.1. Состояние нити. [#229175 #333297]

--- src/main/java/ru/job4j/concurrent/ThreadState.java
+++ src/main/java/ru/job4j/concurrent/ThreadState.java
@@ -1,22 +1,24 @@
 package ru.job4j.concurrent;
 
 public class ThreadState {
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) {
         Thread first = new Thread(
-                () -> System.out.println("first thread - " + Thread.currentThread().getName())
+                () -> System.out.println("First thread - " + Thread.currentThread().getName())
         );
         Thread second = new Thread(
                 () -> System.out.println("Second thread - " + Thread.currentThread().getName())
         );
         System.out.println("first thread - " + first.getState());
+        System.out.println("second thread - " + second.getState());
         first.start();
         second.start();
-        while (first.getState() != Thread.State.TERMINATED) {
+        while (first.getState() != Thread.State.TERMINATED || second.getState()
+                != Thread.State.TERMINATED) {
             System.out.println("first thread - " + first.getState());
+            System.out.println("second thread - " + second.getState());
         }
         System.out.println("first thread - " + first.getState());
-        first.join();
-        second.join();
+        System.out.println("second thread - " + second.getState());
         System.out.println("Main thread - " + Thread.currentThread().getName());
         System.out.println("Job is done");
     }
