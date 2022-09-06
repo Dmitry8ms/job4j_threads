89687d94d0241a9450206915ba389bb78049bd5d
Dmitry8ms <dshatokhin@yandex.ru>
2022-09-06 12:08:36 +0700

    2. Обеспечить остановку потребителя. [#66825 #340076]

---
+++ src/main/java/ru/job4j/synch/ParallelSearch.java
@@ -0,0 +1,35 @@
+package ru.job4j.synch;
+
+public class ParallelSearch {
+
+    public static void main(String[] args) {
+        SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<Integer>(10);
+        final Thread consumer = new Thread(
+                () -> {
+                    while (!Thread.currentThread().isInterrupted()) {
+                        try {
+                            System.out.println(queue.poll());
+                        } catch (InterruptedException e) {
+                            System.out.println("Consumer is interrupted");
+                            Thread.currentThread().interrupt();
+                        }
+                    }
+                }
+        );
+        consumer.start();
+        new Thread(
+                () -> {
+                    for (int index = 0; index != 3; index++) {
+                        try {
+                            queue.offer(index);
+                            Thread.sleep(500);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    consumer.interrupt();
+                }
+
+        ).start();
+    }
+}
