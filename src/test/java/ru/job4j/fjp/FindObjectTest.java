c6bdd15777b525d3c9c1fbf08dfdc62d116bb5ec
Dmitry8ms <dshatokhin@yandex.ru>
2022-09-17 09:48:39 +0700

    3. ForkJoinPool [#315067]

--- src/test/java/ru/job4j/fjp/FindObjectTest.java
+++ src/test/java/ru/job4j/fjp/FindObjectTest.java
@@ -18,4 +18,28 @@ public class FindObjectTest {
         assertEquals(-1, FindObject.find(array, new Model(110)));
     }
 
+    @Test
+    public void whenLessThan10ThenOk() {
+        Model[] array = InitArray.get(9);
+        assertEquals(3, FindObject.find(array, new Model(3)));
+    }
+
+    @Test
+    public void whenLastElementThenOk() {
+        Model[] array = InitArray.get(100);
+        assertEquals(99, FindObject.find(array, new Model(99)));
+    }
+
+    @Test
+    public void whenIntegerElementsThenOk() {
+        Integer[] array = new Integer[]{1, 2, 3, 9, 5};
+        assertEquals(3, FindObject.find(array, Integer.valueOf(9)));
+    }
+
+    @Test
+    public void whenStringElementsThenOk() {
+        String[] array = new String[] {"1", "2", "3", "9", "5"};
+        assertEquals(3, FindObject.find(array, "9"));
+    }
+
 }
