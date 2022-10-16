3af67ca8e2002395fe0cf07855bab436bb15255d
Dmitry8ms <dshatokhin@yandex.ru>
2022-10-16 11:51:29 +0700

    1. Подготовка к экзамену по многопоточности

--- src/main/java/ru/job4j/concurrent/ConcurrentOutput.java
+++ src/main/java/ru/job4j/concurrent/ConcurrentOutput.java
@@ -1,15 +1,23 @@
 package ru.job4j.concurrent;
 
 public class ConcurrentOutput {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         Thread another = new Thread(
-                () -> System.out.println(Thread.currentThread().getName())
+                () -> {
+                    while (!Thread.currentThread().isInterrupted()) {
+                        System.out.println(Thread.currentThread().getName());
+                    }
+
+                }
         );
         Thread second = new Thread(
                 () -> System.out.println(Thread.currentThread().getName())
         );
         another.start();
+        Thread.sleep(1);
         second.start();
+        Thread.sleep(1);
+        another.interrupt();
         System.out.println(Thread.currentThread().getName());
     }
 }
